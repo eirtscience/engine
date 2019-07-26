08bcffcb20a98d6f6fc90195f4bd413b82604ce4
malevae@gmail.com <evarist@eirt.science>
2019-07-27 03:07:28 +0800

    Calculate test case

--- run_test.py
+++ run_test.py
@@ -7,7 +7,7 @@ class RunTest(unittest.TestCase):
         self.assertTrue(run.run_script(),True)
 
     def test_calculate(self):
-        self.assertEqual(run.calculate(3,6),7)
+        self.assertEqual(run.calculate(3,6),9)
         self.assertEqual(run.calculate(3,4),7)
 
 
