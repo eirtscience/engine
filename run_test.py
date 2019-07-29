2540fc7c6b03f411ff0192a8ee27e76dd13fd773
Tlab <malevae@gmail.com>
2019-07-29 18:41:39 +0800

    New Test data

--- run_test.py
+++ run_test.py
@@ -9,6 +9,7 @@ class RunTest(unittest.TestCase):
     def test_calculate(self):
         self.assertEqual(run.calculate(3,6),9)
         self.assertEqual(run.calculate(3,4),7)
+        self.assertEqual(run.calculate(7,4),11)
 
 
 
