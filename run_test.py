6a7ba97d1ef2952b6a28b8cd3adaf1f4998d228b
Tlab <malevae@gmail.com>
2019-08-06 11:31:30 +0800

    New commit

--- run_test.py
+++ run_test.py
@@ -9,7 +9,7 @@ class RunTest(unittest.TestCase):
     def test_calculate(self):
         self.assertEqual(run.calculate(3,6),9)
         self.assertEqual(run.calculate(4,4),9)
-        self.assertEqual(run.calculate(7,13),20)
+        self.assertEqual(run.calculate(7,14),20)
 
 
 
